fe1a2b6bf6aa87822f3c2b138c4cdfaa731aa28c
antmusumba <musumbaantony3@gmail.com>
2024-12-04 15:18:21 +0300

    feat: add function for detting album by ID

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"net/http"           // Provides HTTP server and client functionality.
+	"net/http" // Provides HTTP server and client functionality.
+
 	"github.com/gin-gonic/gin" // Gin framework for building APIs.
 )
 
@@ -25,14 +26,39 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+//getAlbumsById locates the album whose ID value matches the id
+func getAlbumsById(c *gin.Context){
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK,a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 // homeHandler responds with a welcome message.
 func homeHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Welcome to the Albums API! Use /albums to view the list of albums.")
 }
 
 func main() {
-	router := gin.Default()                 // Initialize the router.
-	router.GET("/", homeHandler)            // Route for the root URL.
-	router.GET("/albums", getAlbums)        // Route to get all albums.
-	router.Run("localhost:8080")            // Start the server on localhost:8080.
+	router := gin.Default()          // Initialize the router.
+	router.GET("/", homeHandler)     // Route for the root URL.
+	router.GET("/albums", getAlbums) // Route to get all albums.
+	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id",getAlbumsById)
+	router.Run("localhost:8080")     // Start the server on localhost:8080.
 }
